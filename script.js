0d3b1f0c2253e16d335719e98b88cf5f17038e12
Ailun Shen <newhoopsdynasty@gmail.com>
2024-02-25 12:11:05 -0500

    Hamburger menu fix to be more mobile friendly

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollToContactButton = document.getElementById('scrollToContact');
+    var contactSection = document.getElementById('contact');
+    
+    if (scrollToContactButton && contactSection) {
+        scrollToContactButton.addEventListener('click', function() {
+            contactSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+    var navLinks = document.querySelectorAll('.nav-links a');
+    
+    if (navLinks) {
+        navLinks.forEach(function(link) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                var targetId = this.getAttribute('href').substring(1);
+                var targetElement = document.getElementById(targetId);
+                
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+            });
+        });
+    }
+});
